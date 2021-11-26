15681a9e954ad4edc3b232157de835f52c4e1837
Davide Della Casa <davidedc@gmail.com>
2021-11-26 11:41:49 +0000

    adding more example commands and comments in run-tests-from-master.sh

--- run-tests-from-master.sh
+++ run-tests-from-master.sh
@@ -3,8 +3,21 @@
 # chmod +x "bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh"
 # ./bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh --user
 # export PATH="$PATH:$HOME/bin"
-# tsc --esModuleInterop --downlevelIteration -t es5 run-micro-tests.ts
-# tsc --esModuleInterop --downlevelIteration -t es5 index.ts
+
+# Then
+#   bazel build algebrite
+#   bazel test :all
+# if that doesn't work it's because there might be another bazel installation
+# in which case you can find what's interfering by doing
+#   which bazel
+# and you can run these anyways to run the exact bazel you want:
+#   $HOME/bin/bazel build algebrite
+#   $HOME/bin/bazel test :all
+
+# Also yo ucan build Algebrite without bazel:
+#   tsc --esModuleInterop --downlevelIteration -t es5 run-micro-tests.ts
+#   tsc --esModuleInterop --downlevelIteration -t es5 index.ts
+
 
 cat tests-from-master/*.coffee | coffee -cbs > all-tests-from-master.js
 cat all-tests-from-master.js run-tests-from-master.js | node 
